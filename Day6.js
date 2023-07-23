8578f58f85d3484eb402223ae02ec1e4e3ff032a
Hector Mendoza <hectormauriciomx@gmail.com>
2023-07-23 17:51:11 -0600

    Daily commit

--- Day6.js
+++ Day6.js
@@ -9,28 +9,38 @@ Output: [1,1,2,3,4,4]
 */
 
 var mergeTwoLists = function (list1, list2) {
-  //
+  // Dummy and head set.
   let dummy = new ListNode(0);
-  let head = dummy;
+  let tail = dummy;
 
+  //While both list are not null....
   while (list1 && list2) {
+    // If the value of L1 is lesser than L2
     if (list1.val <= list2.val) {
-      dummy.next = list1;
+      //L1 Value is stored on tail
+      tail.next = list1;
+      //L1 is incremented
       list1 = list1.next;
     } else {
-      dummy.next = list2;
+      //L2 is inserted on tail
+      tail.next = list2;
+      //L2 is incremented
       list2 = list2.next;
     }
-    dummy = dummy.next;
+    // Tail is incremented regardless of both cases
+    tail = tail.next;
   }
 
+  // If there is a case in which one of the list is empty
+  // We find the non-empty list we awe added it to the end of the result.
   if (list1 !== null) {
-    dummy.next = list1;
+    tail.next = list1;
   } else {
-    dummy.next = list2;
+    tail.next = list2;
   }
 
-  return head.next;
+  // Return the list
+  return dummy.next;
 };
 
 mergeTwoLists([1, 2, 4], [1, 3, 4]);
