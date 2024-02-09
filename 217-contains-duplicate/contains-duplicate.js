418cfcedb6d836eb675c8dc06ef5d4f30a941040
Héctor Mendoza <hectormauriciomx@gmail.com>
2024-02-08 21:27:54 -0600

    Time: 86 ms (43.87%) | Memory: 66.4 MB (5.13%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,13 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    numset = new Set()
+    numsmap = new Map()
     for (i = 0; i < nums.length; i++) {
-        if (numset.has(nums[i])) {
+        if (numsmap.has(nums[i])) {
             return true
         }
-        numset.add(nums[i])
+        numsmap.set(nums[i], true)
     }
     return false
-
 };
