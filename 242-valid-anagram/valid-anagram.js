485335999f04dc918f9822e380eae01c98aebaaa
Héctor Mendoza <hectormauriciomx@gmail.com>
2024-02-14 23:42:59 -0600

    Time: 70 ms (66.01%) | Memory: 54.7 MB (6.58%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    if (s.length != t.length) {
+        return false
+    }
+
+    let map = {}
+
+    for (let c of s) {
+        map[c] = (map[c] || 0) + 1
+    }
+
+    for (let c of t) {
+        if (!map[c] || map[c] == 0) return false
+        map[c]--
+    }
+    return true
+};
