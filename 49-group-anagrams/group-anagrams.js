cf4cdbc38c32eea00da5b0eae84c359459775b48
Héctor Mendoza <hectormauriciomx@gmail.com>
2024-03-03 15:09:58 -0600

    Time: 104 ms (83.60%) | Memory: 63.1 MB (36.83%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,16 +2,15 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function (strs) {
-    obj = {}
-    for (let n of strs) {
+var groupAnagrams = function(strs) {
+    let obj = {}
+    for(let n of strs){
         let key = n.split('').sort().join('')
-        if (!obj[key]) {
+        if(!obj[key]){
             obj[key] = [n]
-        } else{
+        }else{
             obj[key].push(n)
         }
     }
-    console.log("obj", obj)
-    return(Object.values(obj))
+    return (Object.values(obj))
 };
