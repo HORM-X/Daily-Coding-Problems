c57a58ffe583f57a3e9d792eb81773ad302ddb64
Héctor Mendoza <hectormauriciomx@gmail.com>
2024-03-17 23:35:12 -0600

    Time: 58 ms (90.88%) | Memory: 52.5 MB (78.42%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,14 +4,13 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let hmap = new Map();
-    for (let key of nums) {
-        hmap.set(key, (hmap.get(key) || 0) + 1);
-    }
-    let sortedEntries = Array.from(hmap.entries()).sort((a, b) => b[1] - a[1]);
-    let result = [];
-    for (let i = 0; i < k; i++) {
-        result.push(sortedEntries[i][0]);
-    }
-    return result;
+    let map = {}
+    
+    for(let n of nums){
+        if(!map[n]){
+            map[n] = 0
+        }
+        map[n]++
+        }
+        return arr = [...Object.keys(map)].sort((a,b) => map[b] - map[a]).slice(0,k)
 };
