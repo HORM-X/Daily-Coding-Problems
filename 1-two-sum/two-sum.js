e51cca46a8423c982b5659b5bf018d3d0ebfa1a5
Héctor Mendoza <hectormauriciomx@gmail.com>
2024-03-20 22:48:40 -0600

    Time: 52 ms (90.54%) | Memory: 51.3 MB (32.69%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    hmap = new Map()
-    for (let i = 0; i < nums.length; i++){
+    map = new Map()
+    for(let i = 0; i < nums.length; i++){
         let n1 = nums[i]
         let n2 = target - n1
-        if(hmap.has(n2)){
-            return  [i, hmap.get(n2)]
-        }
-        hmap.set(n1, i)
+        if(map.has(n2)){
+            return [i, map.get(n2)]
+            }
+        map.set(n1, i)
     }
 };
